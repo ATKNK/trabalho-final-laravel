9aaccb92c4293b9ed2cb100032931663fc89203b
Arthur Nakayama <arthurnakayama@gmail.com>
2025-06-28 15:25:44 -0300

    Feat: Migration e Model do Jogador criados

--- trabalho-final/app/Models/Player.php
+++ trabalho-final/app/Models/Player.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Player extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'players';
+
+    protected $fillable = [
+        'name',
+        'health',
+        'mana',
+        'class',
+    ];
 }
