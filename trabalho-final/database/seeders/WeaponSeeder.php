04c1bd02f67be11ec438048c816beb95ca53fdc1
Arthur Nakayama <arthurnakayama@gmail.com>
2025-07-01 02:57:43 -0300

    Fix: Seeders funcionando, id de classe corrigido

--- trabalho-final/database/seeders/WeaponSeeder.php
+++ trabalho-final/database/seeders/WeaponSeeder.php
@@ -14,10 +14,10 @@ class WeaponSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ["name"=> "Copper Shortsword", "baseDamage"=> 5, "knockback"=> 4, "attackSpeed"=> 13, "classId"=> 1],
-            ["name"=> "Enchanted Sword", "baseDamage"=> 23, "knockback"=> 4.25, "attackSpeed"=> 21, "classId"=> 1],
-            ["name"=> "Bee Keeper", "baseDamage"=> 30, "knockback"=> 5.3, "attackSpeed"=> 20, "classId"=> 1],
-            ["name"=> "Starfury", "baseDamage"=> 25, "knockback"=> 5, "attackSpeed"=> 20, "classId"=> 1],
+            ["name"=> "Copper Shortsword", "baseDamage"=> 5, "knockback"=> 4, "attackSpeed"=> 13, "class_id"=> 1],
+            ["name"=> "Enchanted Sword", "baseDamage"=> 23, "knockback"=> 4.25, "attackSpeed"=> 21, "class_id"=> 1],
+            ["name"=> "Bee Keeper", "baseDamage"=> 30, "knockback"=> 5.3, "attackSpeed"=> 20, "class_id"=> 1],
+            ["name"=> "Starfury", "baseDamage"=> 25, "knockback"=> 5, "attackSpeed"=> 20, "class_id"=> 1],
         ];
         DB::table('weapons')->insert($data);
     }
