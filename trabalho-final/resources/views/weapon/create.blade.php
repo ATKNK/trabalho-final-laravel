440d37333506f259b2cd8699fd8f4f81fc5c0513
Arthur Nakayama <arthurnakayama@gmail.com>
2025-07-04 22:43:39 -0300

    Feat: Estilização melhorada na tela de criação e edição de armas

--- trabalho-final/resources/views/weapon/create.blade.php
+++ trabalho-final/resources/views/weapon/create.blade.php
@@ -1,51 +1,52 @@
 @extends('layout.base')
 @section('content')
-<div class="flex flex-col justify-center items-center">
-    <div class="w-full flex flex-row justify-between items-center">
-        <h1 class="text-4xl">Creating a Weapon:</h1>
-        <a class="bg-sky-600 p-3 m-1 rounded-xl" href="{{ route('weapons.index') }}">Back</a>
+    <div class="flex flex-col justify-center items-center">
+        <div class="w-full flex flex-row justify-between items-center">
+            <h1 class="text-4xl">Creating a Weapon:</h1>
+            <a class="bg-sky-600 p-3 m-1 rounded-xl" href="{{ route('weapons.index') }}">Back</a>
+        </div>
+
+        <div class="flex flex-col items-center">
+            <form class="w-xl" action="{{ route('weapons.store') }}" method="POST">
+                @csrf
+
+                <div class="bg-sky-800 p-3 m-6 md:w-4/5 rounded-xl">
+                    <div class="flex flex-row justify-between m-3">
+                        <label class="text-lg" for="name">Name: </label>
+                        <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="name" required>
+                    </div>
+
+                    <div class="flex flex-row justify-between m-3">
+                        <label class="text-lg" for="baseDamage">Base Damage: </label>
+                        <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="baseDamage" required>
+                    </div>
+
+                    <div class="flex flex-row justify-between m-3">
+                        <label class="text-lg" for="knockback">Knockback: </label>
+                        <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="knockback" required>
+                    </div>
+
+                    <div class="flex flex-row justify-between m-3">
+                        <label class="text-lg" for="attackSpeed">Attack Speed: </label>
+                        <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="attackSpeed" required>
+                    </div>
+
+                    <div class="flex flex-row justify-between m-3">
+                        <label class="text-lg" for="classId">Class: </label>
+                        <select class="border-1 border-stone-950 bg-stone-800 rounded-xl p-1" name="classId" required>
+                            <option value="">Select a class</option>
+                            @foreach ($classes as $class)
+                                <option value="{{ $class->id }}">
+                                    {{ $class->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <button class="bg-sky-600 p-3 rounded-xl cursor-pointer" style="text-shadow: 2px 2px 2px black" type="submit">Save</button>
+            </form>
+        </div>
+
+
     </div>
-
-    <div class="flex flex-col w-5/7">
-        <form action="{{ route('weapons.store') }}" method="POST">
-            @csrf
-
-            <div class="bg-sky-800 p-3 m-6 md:w-4/5 rounded-xl">
-                <div class="flex flex-row justify-between m-3">
-                    <label class="text-lg" for="name">Name: </label>
-                    <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="name" required>
-                </div>
-
-                <div class="flex flex-row justify-between m-3">
-                    <label class="text-lg" for="baseDamage">Base Damage: </label>
-                    <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="baseDamage" required>
-                </div>
-
-                <div class="flex flex-row justify-between m-3">
-                    <label class="text-lg" for="knockback">Knockback: </label>
-                    <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="knockback" required>
-                </div>
-
-                <div class="flex flex-row justify-between m-3">
-                    <label class="text-lg" for="attackSpeed">Attack Speed: </label>
-                    <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="attackSpeed" required>
-                </div>
-
-                <div class="flex flex-row justify-between m-3">
-                    <label class="text-lg" for="classId">Class: </label>
-                    <select class="border-1 border-stone-950 bg-stone-800 rounded-xl p-1" name="classId" required>
-                        <option value="">Select a class</option>
-                        @foreach ($classes as $class)
-                            <option value="{{ $class->id }}">
-                                {{ $class->name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-
-            <button class="bg-sky-600 p-3 rounded-xl cursor-pointer" style="text-shadow: 2px 2px 2px black" type="submit">Save</button>
-        </form>
     </div>
-</div>
 @endsection
--- trabalho-final/resources/views/weapon/edit.blade.php
+++ trabalho-final/resources/views/weapon/edit.blade.php
@@ -1,55 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Editing a weapon:</h1>
-    <form action="{{ route('weapons.update', $weapon->id) }}" method="POST">
-        @method('PUT')
-        @csrf
-
-        <div>
-            <div>
-                <label for="name">Name: </label>
-                <input type="text" name="name" value="{{ $weapon->name }}" required>
-            </div>
-
-            <div>
-                <label for="baseDamage">Base Damage: </label>
-                <input type="text" name="baseDamage" value="{{ $weapon->baseDamage }}" required>
-            </div>
-
-            <div>
-                <label for="knockback">Knockback: </label>
-                <input type="text" name="knockback" value="{{ $weapon->knockback }}" required>
-            </div>
-
-            <div>
-                <label for="attackSpeed">Attack Speed: </label>
-                <input type="text" name="attackSpeed" value="{{ $weapon->attackSpeed }}" required>
-            </div>
-
-            <div>
-                <label for="classId">Class: </label>
-                <select name="classId" required>
-                    <option value="">Select a class</option>
-                    @foreach ($classes as $class)
-                        <option value="{{ $class->id }}">
-                            {{$class->name}}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
+@extends('layout.base')
+@section('content')
+    <div class="flex flex-col justify-center items-center">
+        <div class="w-full flex flex-row justify-between items-center">
+            <h1 class="text-4xl">Editing a Weapon:</h1>
+            <a class="bg-sky-600 p-3 m-1 rounded-xl" href="{{ route('weapons.index') }}">Back</a>
         </div>
 
-        <button type="submit">Save</button>
-    </form>
-</body>
+        <div class="flex flex-col items-center">
+        <form class="w-xl" action="{{ route('weapons.update', $weapon->id) }}" method="POST">
+            @method('PUT')
+            @csrf
 
-</html>
+            <div class="bg-sky-800 p-3 m-6 md:w-4/5 rounded-xl">
+                <div class="flex flex-row justify-between m-3">
+                    <label class="text-lg" for="name">Name: </label>
+                    <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="name" value="{{ $weapon->name }}" required>
+                </div>
+
+                <div class="flex flex-row justify-between m-3">
+                    <label class="text-lg" for="baseDamage">Base Damage: </label>
+                    <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="baseDamage" value="{{ $weapon->baseDamage }}" required>
+                </div>
+
+                <div class="flex flex-row justify-between m-3">
+                    <label class="text-lg" for="knockback">Knockback: </label>
+                    <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="knockback" value="{{ $weapon->knockback }}" required>
+                </div>
+
+                <div class="flex flex-row justify-between m-3">
+                    <label class="text-lg" for="attackSpeed">Attack Speed: </label>
+                    <input class="border-1 border-stone-950 rounded-xl p-1" type="text" name="attackSpeed" value="{{ $weapon->attackSpeed }}" required>
+                </div>
+
+                <div class="flex flex-row justify-between m-3">
+                    <label class="text-lg" for="classId">Class: </label>
+                    <select class="border-1 border-stone-950 bg-stone-800 rounded-xl p-1" name="classId" required>
+                        <option value="">Select a class</option>
+                        @foreach ($classes as $class)
+                            <option value="{{ $class->id }}">
+                                {{ $class->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <button class="bg-sky-600 p-3 rounded-xl cursor-pointer" style="text-shadow: 2px 2px 2px black" type="submit">Save</button>
+            </div>
+        </form>
+        </div>
+    </div>
+@endsection
