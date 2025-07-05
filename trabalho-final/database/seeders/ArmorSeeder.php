640628936b194865d0f8b72c17e64e4820b27e68
Arthur Nakayama <arthurnakayama@gmail.com>
2025-07-05 16:33:53 -0300

    Feat: Duas armadruas adicionadas ao seeder

--- trabalho-final/database/seeders/ArmorSeeder.php
+++ trabalho-final/database/seeders/ArmorSeeder.php
@@ -15,7 +15,9 @@ class ArmorSeeder extends Seeder
         $data = [
             ["name" => "Wood Armor", "setBonus" => "defense", "setBonusModifier" => 1, "defense" => 3, 'rarity' => 0, "classId" => 1],
             ["name" => "Necro Armor", "setBonus" => "Ranged Damage", "setBonusModifier" => 10, "defense" => 19, 'rarity' => 2, "classId" => 2],
-            ["name" => "Hallowed Armor", "setBonus" => "Dodge", "setBonusModifier" => 1, "defense" => 35, 'rarity' => 5, "classId" => 2]
+            ["name" => "Hallowed Armor", "setBonus" => "Dodge", "setBonusModifier" => 1, "defense" => 35, 'rarity' => 5, "classId" => 2],
+            ["name" => "Meteor Armor", "setBonus" => "Space Gun no cost", "setBonusModifier" => 1, "defense" => 16, 'rarity' => 1, "classId" => 3],
+            ["name" => "Spider Armor", "setBonus" => "Summon Damage", "setBonusModifier" => 12, "defense" => 20, 'rarity' => 4, "classId" => 4],
         ];
         DB::table('armors')->insert($data);
     }
