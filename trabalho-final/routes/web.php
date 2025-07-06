36ea1552c3e8afe047b70f20072983411e77da4b
Arthur Nakayama <arthurnakayama@gmail.com>
2025-07-05 22:27:59 -0300

    Chore: Instalação do Breeze

--- trabalho-final/routes/web.php
+++ trabalho-final/routes/web.php
@@ -1,16 +1,27 @@
 <?php
 
-use App\Http\Controllers\AccessoryController;
-use App\Http\Controllers\PlayerController;
-use App\Http\Controllers\SetupController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PlayerClassController;
 use App\Http\Controllers\WeaponController;
 use App\Http\Controllers\ArmorController;
+use App\Http\Controllers\AccessoryController;
+use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\SetupController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::resource('/classes', PlayerClassController::class);
@@ -19,3 +30,5 @@ Route::resource('/armors', ArmorController::class);
 Route::resource('/accessories', AccessoryController::class);
 Route::resource('/players', PlayerController::class);
 Route::resource('/setups', SetupController::class);
+
+require __DIR__.'/auth.php';
